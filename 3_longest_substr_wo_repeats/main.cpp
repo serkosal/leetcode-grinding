3a7d944a8dbe0a456f4e6b7a4045e635bbefd0cd
Sergey Kosykh <s3rkosal@gmail.com>
2026-06-04 23:42:00 +0400

    added cpp solution of the 3rd problem

---
+++ 3_longest_substr_wo_repeats/main.cpp
@@ -0,0 +1,41 @@
+#include <string>
+#include <iostream>
+#include <map>
+
+using std::string, std::map;
+
+int lengthOfLongestSubstring(const string& str) {
+    int sz = str.size();
+    int max_size = 0;
+        
+    for (int left = 0; left < sz - max_size; ) {
+
+        int right = std::min(sz, left + 100);
+
+        map<char, int> char_map;
+
+        while (left < right) {
+            char ch = str[left];
+            if (char_map.contains(ch)) {
+                left = char_map[ch] + 1; 
+                break;
+            }
+
+            char_map[ch] = left++;
+        }
+
+        if (char_map.size() > max_size)
+            max_size = char_map.size();
+    }
+
+    return max_size;
+}
+
+int main() {
+
+    string s = "abcabcbb";
+
+    std::cout << lengthOfLongestSubstring(s) << "\n";
+
+    return 0;
+}
